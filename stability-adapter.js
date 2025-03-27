6ce111afca2a431b02ce2d8f3615e94a9d988454
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 15:36:34 -0700

    tweaks

--- stability-adapter.js
+++ stability-adapter.js
@@ -1,5 +1,41 @@
+import fetch from 'node-fetch';
+import uuid from 'uuid';
+
 export default Adapter {
-  constructor(configuration) {
+  constructor({ apiKey, home }) {
+    this.apiKey = apiKey;
+    this.home = home || 'data/images';
+  }
+
+  async imagine(prompt) {
+    const payload = {
+      text_prompts: [{
+        text: prompt,
+        weight: 1.0
+      }],
+      height: 960,
+      width: 1344
+    };
     
+    const fetched = await fetch(
+      'https://api.stability.ai/v1/generation/stable-diffusion-v1-6/text-to-image',
+      {
+        body: JSON.stringify(payload),
+        headers: {
+          Accept: 'image/png',
+          Authorization: this.apiKey,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }
+    );
+    const filename = path.join(this.home, `${uuid.v4()}.png`);
+    const stream = createWriteStream(filename);
+    await new Promise((resolve, reject) => {
+      fetched.body.pipe(stream);
+      fetched.body.on('error', reject);
+      stream.on('finish', resolve);
+    });
+    return filename;    
   }
 }
