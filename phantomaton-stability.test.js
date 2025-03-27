dd6459a68f31079f5228b99af4dea795422a4296
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-27 16:17:18 -0700

    Updated by Phantomaton

--- phantomaton-stability.test.js
+++ phantomaton-stability.test.js
@@ -1,11 +1,64 @@
-import { expect } from 'lovecraft';
-import stabilityAdapter from './adapter.js';
+import { expect, stub } from 'lovecraft';
+import fs from 'fs';
+import path from 'path';
+import * as uuid from 'uuid';
+import fetch from 'node-fetch';
+
+import Adapter from './adapter.js';
 
 describe('stabilityAdapter', () => {
-  it('does call the imagine method and return a path', async () => {
+  let fetchStub, createWriteStreamStub, uuidStub;
+  const apiKey = 'test-api-key';
+  const home = 'test-images';
+  let adapter;
+
+  beforeEach(() => {
+    fetchStub = stub(fetch, 'default');
+    createWriteStreamStub = stub(fs, 'createWriteStream');
+    uuidStub = stub(uuid, 'v4').returns('test-uuid');
+    adapter = new Adapter({ apiKey, home });
+  });
+
+  afterEach(() => {
+    fetchStub.restore();
+    createWriteStreamStub.restore();
+    uuidStub.restore();
+  });
+
+  it('does call the Stability AI API and save the image to a file', async () => {
     const prompt = 'A cat riding a unicorn';
-    const imagePath = await stabilityAdapter.imagine(prompt);
-    expect(imagePath).to.be.a('string');
-    expect(imagePath).to.not.be.empty;
+    const mockResponse = {
+      body: {
+        pipe: stub().returnsThis(),
+        on: stub().yields()
+      }
+    };
+    fetchStub.resolves(mockResponse);
+    createWriteStreamStub.returns({
+      on: stub().yields()
+    });
+
+    const imagePath = await adapter.imagine(prompt);
+
+    expect(fetchStub).toHaveBeenCalledWith(
+      'https://api.stability.ai/v1/generation/stable-diffusion-v1-6/text-to-image',
+      {
+        body: JSON.stringify({
+          text_prompts: [{ text: prompt, weight: 1.0 }],
+          height: 960,
+          width: 1344
+        }),
+        headers: {
+          Accept: 'image/png',
+          Authorization: apiKey,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }
+    );
+
+    const expectedFilename = path.join(home, 'test-uuid.png');
+    expect(createWriteStreamStub).toHaveBeenCalledWith(expectedFilename);
+    expect(imagePath).to.eq(expectedFilename);
   });
 });
