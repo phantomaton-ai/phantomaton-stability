95b246615fcd4b869a585529a87df661bbeed7d8
Doc Woe <woe@phantomaton.com>
2025-05-16 19:31:02 +0000

    options-fix

--- adapter.js
+++ adapter.js
@@ -7,8 +7,8 @@ export default class Adapter {
   constructor({ apiKey, home, height, width }) {
     this.apiKey = apiKey;
     this.home = home || 'data/images';
-    this.width = 1344;
-    this.height = 960;
+    this.width = width || 1344;
+    this.height = height || 960;
   }
 
   async imagine(prompt, parameters = {}) {
@@ -17,7 +17,7 @@ export default class Adapter {
       height: parameters.height || this.height || 960,
       width: parameters.width || this.width || 1344
     };
-    
+
     const fetched = await util.fetch(
       'https://api.stability.ai/v1/generation/stable-diffusion-v1-6/text-to-image',
       {
@@ -37,6 +37,6 @@ export default class Adapter {
       fetched.body.on('error', reject);
       stream.on('finish', resolve);
     });
-    return filename;    
+    return filename;
   }
 }
